8bdb84d746eda068f3d9038dd7b49098fb1d7d3e
mohamad-azizzadeh81 <94900086+mohamad-azizzadeh81@users.noreply.github.com>
2022-04-17 11:01:05 +0430

    Create indax2.py

---
+++ indax2.py
@@ -0,0 +1,23 @@
+import colorsys
+import turtle
+
+
+from turtle import *
+from colorsys import *
+
+hue = 0.8
+bgcolor("black")
+speed(0)
+
+for i in range(264):
+    col =hsv_to_rgb(hue,1,1)
+    hue +=0.005
+    color(col,col)
+    begin_fill()
+    circle(278 -i,90)
+    lt(90)
+    circle(270 -i,90)
+    lt(10)
+    end_fill()
+
+done()
